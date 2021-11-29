be02869a879fd0fd0176e673263a2cad9f84b1b4
Lino Estêvão <linoestevao461@gmail.com>
2021-11-29 23:29:03 +0000

    fix nodered worker health check

--- src/nodered/nodered.service.ts
+++ src/nodered/nodered.service.ts
@@ -1,7 +1,7 @@
 import { INestApplication, Injectable, Logger } from '@nestjs/common';
 import { NodeRedWorkerSettings } from './nodered.settings.interface';
 
-import {Worker, isMainThread, parentPort, workerData} from 'worker_threads';
+import { Worker, isMainThread, parentPort, workerData } from 'worker_threads';
 import { setInterval } from 'timers';
 
 const path = require('path');
@@ -14,47 +14,58 @@ const noderedWorker = path.resolve(__dirname, 'node-red.worker.js');
 export class NoderedService {
 
   private nodeRedWorker: Worker;
-    private healthcheckIntervalTimeout;
-    private healthcheckInterval;
+  private healthcheckInterval;
+  private healthcheckIntervalTimeout;
+  private app;
+  private settings;
 
+  private readonly logger = new Logger(NoderedService.name);
+
+  constructor() { }
+
+  init(app: INestApplication | any, settings: NodeRedWorkerSettings) {
+    this.app = app;
+    this.settings = settings;
+
+    this.nodeRedWorker = new Worker(noderedWorker, {
+      workerData: settings
+    });
+
+    this.nodeRedWorker.on('message', (msg) => {
+      if ('pong' == msg) {
+        this.startHealthCheck();
+      } else {
+        this.logger.debug(msg)
+      }
+    });
+    this.nodeRedWorker.on('error', (msg) => this.logger.error(msg));
+
+    this.nodeRedWorker.on('exit', code => {
+      if (code !== 0) this.logger.error(new Error(`Worker stopped with exit code ${code}`));
+    });
+
+    this.healthcheckInterval = setInterval(() => {
+      this.nodeRedWorker.postMessage('ping');
+    }, 1000);
     
-    private readonly logger = new Logger(NoderedService.name);
+  }
 
-    constructor( ) { }
+  startHealthCheck() {
+    clearTimeout(this.healthcheckIntervalTimeout);
+    this.healthcheckIntervalTimeout = setTimeout(() => {
+      this.nodeRedWorker.terminate();
+      this.stopHealthCheck();
+      this.logger.warn("NODERED ping timeout, rebooting in 3s");
+        setTimeout(() => {
+          this.init(this.app, this.settings);
+        }, 3000);
+    }, 10000);
+    
+  }
 
-    init(app: INestApplication | any, settings: NodeRedWorkerSettings) {
-      
-      this.nodeRedWorker = new Worker(noderedWorker, {
-        workerData: settings
-      });
-      
-      this.nodeRedWorker.on('message', (msg) => {
-        if('pong' == msg) {
-          clearTimeout(this.healthcheckIntervalTimeout);
-          console.log('clear timeout')
-        } else {
-          this.logger.debug(msg)
-        }
-      });
-      this.nodeRedWorker.on('error', (msg) => this.logger.error(msg));
-      this.nodeRedWorker.on('exit', code => {
-        if (code !== 0) this.logger.error(new Error(`Worker stopped with exit code ${code}`));
-      });
-      
-
-      this.healthcheckInterval = setInterval(() => {
-        this.nodeRedWorker.postMessage('ping');
-        
-        this.healthcheckIntervalTimeout = setTimeout(() => {
-          this.nodeRedWorker.terminate();
-          clearInterval(this.healthcheckInterval);
-          this.logger.warn("NODERED ping timeout, rebooting in 3s");
-          setTimeout(() => {
-            this.init(app, settings);
-          }, 5000);
-        })
-
-      }, 5000);
-
-    }
+  stopHealthCheck() {
+    clearTimeout(this.healthcheckIntervalTimeout);
+    clearInterval(this.healthcheckInterval);
+  }
+  
 }
