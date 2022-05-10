ab87907e382bb3a42616cc257a22d25555b1d2ef
Carlos Almeida <carlos.almeida@productdock.com>
2022-05-10 14:08:52 +0100

    new build

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ import { Logger } from '@nestjs/common';
 
 
 async function bootstrap() {
+  
   const app = await NestFactory.create(AppModule);
   const configService = app.get<ConfigService>(ConfigService);
   const noderedModule = app.get<NoderedModule>(NoderedModule);
