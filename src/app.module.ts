e229a2e23ff0657884a6aecc4bcf7a46d66ded9b
Carlos Almeida <carlos.almeida@productdock.com>
2022-06-29 16:50:08 +0100

    housekeeper changed to their own module

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { NoderedModule } from './nodered/nodered.module';
 import { AuthModule } from './auth/auth.module';
 import { SessionService } from './session/session.service';
 import { TrackService } from './track/track/track.service';
-import { Cron, ScheduleModule } from '@nestjs/schedule';
+import { ScheduleModule } from '@nestjs/schedule';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { MetricsModule } from './metrics/metrics.module';
@@ -60,30 +60,6 @@ export class AppModule {
   ) {
     this.runMigrations();
   }
-
-  @Cron('*/10 * * * * *')
-  async handleCron() {
-    //TODO: Use database instead
-    const startDate = new Date();
-    startDate.setFullYear(1970);
-    const endDate = new Date();
-    endDate.setMonth(endDate.getMonth() - +this.configService.get('TRACK_LIFETIME_MONTHS'));
-
-    let userSessions = await this.sessionService.getSessionsByDate(0, 10000, startDate, endDate);
-    for (let us of userSessions) {
-      this.logger.debug(`[housekeeper] removing expired sid ${us.sid}`);
-      await this.sessionService.removeSession(us);
-    }
-
-    let clientTracks = await this.trackService.getSessionsByDate(0, 1000, startDate, endDate);
-
-    for (let ct of clientTracks) {
-      this.logger.debug(`[housekeeper] removing expired tid ${ct.sid}`);
-      await this.trackService.removeClientTrack(ct);
-    }
-
-  }
-
 
   // migrations
 
--- src/track/track/track.module.ts
+++ src/track/track/track.module.ts
@@ -1,13 +1,41 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { TrackService } from './track.service';
 import { TrackController } from './track.controller';
 import { ArangoModule } from 'src/persistence/arango/arango.module';
 import { AuthModule } from 'src/auth/auth.module';
+import { Cron } from '@nestjs/schedule';
+import { ConfigModule } from '../../config/config/config.module';
+import { ConfigService } from '../../config/config/config.service';
+
 
 @Module({
-  imports: [AuthModule, ArangoModule.collection('track')],
+  imports: [AuthModule, ConfigModule, ArangoModule.collection('track')],
   providers: [TrackService],
   controllers: [TrackController],
   exports: [TrackService]
 })
-export class TrackModule {}
+export class TrackModule {
+
+  private readonly logger = new Logger(TrackModule.name);
+  constructor(
+    private configService: ConfigService,
+    private readonly trackService: TrackService
+  ) {
+    
+  }
+
+  @Cron('*/10 * * * *')
+  async handleCron() {
+    const startDate = new Date();
+    startDate.setFullYear(1970);
+    const endDate = new Date();
+    endDate.setMonth(endDate.getMonth() - +this.configService.get('TRACK_LIFETIME_MONTHS'));
+    let clientTracks = await this.trackService.getSessionsByDate(0, 1000, startDate, endDate);
+
+    for(let ct of clientTracks) {
+      this.logger.debug(`[housekeeper] removing expired tid ${ct.sid}`);
+      await this.trackService.removeClientTrack(ct);
+    }
+
+  }
+}
